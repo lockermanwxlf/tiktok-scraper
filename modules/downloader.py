124b22eaf9c2e1c911d21c350575757ea44bce60
lockermanwxlf <jcbounds05@gmail.com>
2024-02-29 19:34:35 -0600

    start download thread, sleep between downloads

--- modules/downloader.py
+++ modules/downloader.py
@@ -3,6 +3,7 @@ from threading import Thread
 import pandas as pd
 from downloaders.savetik import SavetikDownloader
 from modules.path_generator import PathGenerator
+from time import sleep
 
 def consumer(downloader):
     print("Download thread started.")
@@ -12,13 +13,16 @@ def consumer(downloader):
     downloader_index = 0
     while True:
         post_id, directory = downloader.queue.get()
+        print(post_id, directory)
         path_generator = PathGenerator(directory)
         print(f'Download {post_id} to {directory}')
         downloaders[downloader_index].download_tiktok(f'https://tiktok.com/@/video/{post_id}', path_generator.filepath(post_id))
         downloader.df = downloader.df[downloader.df['Post Id'] != post_id]
         downloader.df.to_csv('data/download_queue.csv', index = False)
-        print('Downloaded')
+        sleep_time = 15 / len(downloaders)
+        print(f'Downloaded. Waiting {int(sleep_time*10+0.5)/10} to avoid getting blocked.')
         downloader_index = (downloader_index + 1) % len(downloaders)
+        sleep(sleep_time)
         
 class Downloader:
     def __init__(self) -> None:
@@ -26,7 +30,7 @@ class Downloader:
         self.df = pd.read_csv('data/download_queue.csv')
         for post_id, directory in zip(self.df['Post Id'], self.df['Directory']):
             self.queue.put((post_id, directory))
-        self.thread = Thread(target=consumer, args=[self])
+        self.thread = Thread(target=consumer, args=[self]).start()
         
     def enqueue(self, post_id: str, directory: str):
         if (len(self.df.loc[self.df['Post Id'] == post_id])>0):
